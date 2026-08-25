1596dd616521f49730ebc29be6b5038a55eca938
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2026-08-25 05:43:31 +0000

    🔧 nvim: opt in to OSC 52 clipboard so yanks reach the local machine over SSH

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- SSH 越しでも + レジスタを端末側クリップボードへ (:h clipboard-osc52)
+-- LazyVim は SSH 時に clipboard='' にするが、g:termfeatures.osc52 が立たない端末では
+-- provider が選ばれないため明示的に opt-in する
+vim.g.clipboard = "osc52"
