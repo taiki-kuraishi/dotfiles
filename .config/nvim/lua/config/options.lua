fffffd66a5b357ba8550a276c54167a71a8fcf2e
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2026-09-02 10:27:11 +0000

    🔧 nvim: sync unnamed yanks over OSC52 with clipboard=unnamedplus

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,8 @@
 -- LazyVim は SSH 時に clipboard='' にするが、g:termfeatures.osc52 が立たない端末では
 -- provider が選ばれないため明示的に opt-in する
 vim.g.clipboard = "osc52"
+
+-- LazyVim は SSH 時に 'clipboard' を空にして自動同期(unnamedplus)を切るため、
+-- yy 等の無名ヤンクが + レジスタに同期されず、上記の osc52 provider が呼ばれない。
+-- 明示的に unnamedplus に戻し、OSC52 経由で自動同期させる。
+vim.opt.clipboard = "unnamedplus"
